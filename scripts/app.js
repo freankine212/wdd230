ba8cc31bc35c1bc5dfddb9f70a71a7494d15c26f
freankine212 <lilmichaeljr@gmail.com>
2023-01-26 21:02:31 -0700

    changed apps for all 3 projects, adjusted footer

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 const date = new Date(document.lastModified);
 document.getElementById("date").innerHTML = date; 
 
-
-
+document.getElementById("theCurrentYear").innerHTML = new
+   Date().getFullYear();
