20ef41a0bb398e5756b78f1fd0cdb4d2d2643c29
freankine212 <lilmichaeljr@gmail.com>
2023-02-03 12:58:22 -0700

    w5 added chamber javascript
    Co-authored-by: crewsbd <crewsbd@users.noreply.github.com>

--- chamber/scripts/apps.js
+++ chamber/scripts/apps.js
@@ -11,3 +11,33 @@ document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-US
 	dateStyle: "full"}).format(currentDate);
 
 
+const weekday = [
+   "Sunday",
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thursday",
+   "Friday",
+   "Saturday"
+]
+var day = weekday[date.getDay()];
+
+
+if (day == "Monday" | day == "Tuesday") { 
+   document.getElementById("day").innerHTML ='Join us on Wednesday to meet the towns heroes who fought valiantly in the Battle of the Five Armies against the elves, dwarves, goblins and eagles!';
+}
+else if (day == "Wednesday"|day == "Thursday"|day == "Friday"| day == "Saturday"| day == "Sunday") {
+   document.getElementById("day").innerHTML='Nothing to see here citizen, check back next Monday or Tuesday';
+   }
+
+/*
+if (day == weekday("Tuesday")){
+   document.writeIn('<p>Join us on Wednesday to meet the towns heroes who fought valiantly in the Battle of the Five Armies against the elves, dwarves, goblins and eagles!</p>');
+}
+*/
+
+
+   //document.getElementById(id).innerHTML = "Nothing to see...";
+
+
+   /* gotcha */
